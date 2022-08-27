8b13e0a5dc842c21112f1ff76fdfe97fc01903bc
jehianth <jehianathayata@gmail.com>
2022-08-27 13:48:37 +0700

    first commit

---
+++ windows/AplikasiPertama/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::AplikasiPertama::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::AplikasiPertama::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
