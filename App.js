2b47a80590586f50b1db2a5eab99641183a9461e
jehianth <jehianathayata@gmail.com>
2022-08-28 14:44:53 +0700

    Styling Component

--- App.js
+++ App.js
@@ -1,112 +1,148 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+/* eslint-disable react-native/no-inline-styles */
+// 3 bagian sederhana
+// import, main code, export
+import React, {Component} from 'react';
+import {Text, StyleSheet, View, Image, TextInput} from 'react-native';
+import msi from './msi.png';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+//satu function hanya bisa mereturn satu component, jadi lebih baik dibungkus pake view
+const App = () => {
+  return <StyleComponent />;
+};
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+//styling di component
+const StyleComponent = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text style={styles.text}>Style Component</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: '#0abde3',
+          borderWidth: 2,
+          borderColor: 'purple',
+          marginTop: 20,
+          marginLeft: 20,
+          marginBottom: 40,
+        }}
+      />
+      <View
+        style={{
+          width: 212,
+          padding: 12,
+          borderRadius: 8,
+          backgroundColor: '#F2F2F2',
+        }}>
+        <Image
+          source={msi}
+          style={{width: 188, height: 107, borderRadius: 8}}
+        />
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: 'bold',
+            color: 'black',
+            marginTop: 16,
+          }}>
+          Jual MSI GF63 10UC-687ID - Gaming Laptop
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: '#F2994A',
+            marginTop: 12,
+          }}>
+          Rp. 11.399.999
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: '300',
+            color: '#5c5c5c',
+            marginTop: 12,
+          }}>
+          Purwokerto Barat
+        </Text>
+        <View
+          style={{
+            backgroundColor: '#6fcf97',
+            paddingVertical: 6,
+            borderRadius: 25,
+            marginTop: 20,
+          }}>
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: '600',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            BELI
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontFamily: 'Plus Jakarta Sans',
+    color: 'black',
+  },
+});
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+//basic component
+const SampleComponent = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}} />
+      <Text style={styles.teks}>Jehian</Text>
+      <Text style={styles.teks}>Athaya</Text>
+      <Prodi />
+      <Photo />
+      <TextInput style={{borderWidth: 1, width: 300}} />
+      <BoxGreen />
+      <Profile />
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const Prodi = () => {
+  return <Text>S1 Informatika</Text>;
+};
+
+const Photo = () => {
+  return (
+    <Image
+      source={{uri: 'https://placeimg.com/300/100/tech'}}
+      style={{width: 300, height: 100}}
+    />
+  );
+};
+
+class BoxGreen extends Component {
+  render() {
+    return <Text>Ini component class</Text>;
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={{uri: 'https://placeimg.com/300/300/people'}}
+          style={{width: 300, height: 300, borderRadius: 150}}
+        />
+        <Text style={{color: 'purple', fontSize: 20}}>Gambar</Text>
+      </View>
+    );
+  }
+}
 
 export default App;
